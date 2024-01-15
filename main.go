5491343fdfcf661974ef425e67f444b4c5e854c7
Emjek Cordover <git@mjec.net>
2024-01-14 22:43:21 -0500

    Output from CoPilot
    Prompt: make the redirect only happen if the request has a host header set to "www.example.com"; otherwise close the connection immediately

--- main.go
+++ main.go
@@ -11,5 +11,13 @@ func main() {
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Host == "www.example.com" {
+		http.Redirect(w, r, "http://new-server.com", http.StatusMovedPermanently)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "http://new-server.com", http.StatusMovedPermanently)
 }
