853958d8c8e2ec7fab2ec506b0cd30d7e2d9aece
Emjek Cordover <git@mjec.net>
2024-01-14 22:54:08 -0500

    Output from CoPilot
    Prompt: add a feature which loads a configuration file containing one or more records called domains. Each domain contains a string called origin, a string called destination, and an integer between 300 and 399 (inclusive) called code

--- main.go
+++ main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 )
 
+type DomainRecord struct {
+	Origin      string `json:"origin"`
+	Destination string `json:"destination"`
+	Code        int    `json:"code"`
+}
+
+type Config struct {
+	Domains []DomainRecord `json:"domains"`
+}
+
+var config Config
+
 func main() {
+	loadConfig("config.json")
+
 	http.HandleFunc("/", redirectHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func loadConfig(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
