19651b1b17d429a199809bbf149ae89cd408b267
Emjek Cordover <git@mjec.net>
2024-01-14 22:51:10 -0500

    Make generated code a bit better, compile

--- main.go
+++ main.go
@@ -14,10 +14,7 @@ func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Host == "www.example.com" {
 		http.Redirect(w, r, "http://new-server.com", http.StatusMovedPermanently)
 	} else {
+		// TODO: make this just close the connection, not even return an HTTP response
 		w.WriteHeader(http.StatusUnauthorized)
 	}
-}
-
-func redirectHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "http://new-server.com", http.StatusMovedPermanently)
 }
